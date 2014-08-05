bfcf667601b496464d28a86bb8b60c5cd81ff969
Margaret Godowns <margaretgodowns@gmail.com>
2014-08-04 23:17:44 -0400

    final array

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ var gitProfile = [
   name: "Will Gallup",
   avatar_URL: "https://avatars.githubusercontent.com/u/7799207?v=2",
   followers: 6,
-  folliwing: 0,
+  following: 0,
   repos: 14
 },
 {
   name: "Ansley Jones",
   avatar_URL: "https://avatars.githubusercontent.com/u/8247044?v=2",
   followers: 8,
-  folliwing: 9,
+  following: 9,
   repos: 6
 },
 {
   name: "Chris Rotten",
   avatar_URL:"https://avatars.githubusercontent.com/u/8238886?v=2",
   followers: 9,
-  folliwing: 40,
+  following: 40,
   repos: 6
 },
 {
   name: "Charles Nguyen",
   avatar_URL: "https://avatars.githubusercontent.com/u/8241367?v=2",
   followers: 7,
-  folliwing: 16,
+  following: 16,
   repos: 9
 },
 {
   name: "Andrew Levy",
   avatar_URL: "https://avatars.githubusercontent.com/u/8238273?v=2",
   followers: 4,
-  folliwing: 0,
+  following: 0,
   repos: 8
 }
 ];
